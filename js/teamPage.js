e7b871592be5baf7270e149578b0bbf8abdb2fc5
Varad Dhumale <varaddhumale177@gmail.com>
2024-10-26 20:42:29 +0530

    team member page betterment

--- js/teamPage.js
+++ js/teamPage.js
@@ -2,8 +2,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 let teamContainer = document.querySelector(".team-container");
 
-
-
 gsap.to(teamContainer, {
   x: () => -(teamContainer.scrollWidth - document.documentElement.clientWidth),
   ease: "none",
@@ -12,6 +10,23 @@ gsap.to(teamContainer, {
     start: "top top",
     end: () => "+=" + teamContainer.scrollWidth,
     scrub: true,
-    pin: ".member-container"
+    pin: ".member-container",
   },
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Left section animation
+  gsap.from(".team-member", {
+    scrollTrigger: {
+      trigger: ".team-member",
+      start: "top 80%",
+      end: "top 40%",
+      scrub: true,
+      markers: true,
+      scroller: "body",
+    },
+    scale: 0,
+    y: 0, // Move to original position
+    duration: 0.1, // Animation duration // Duration for the animation effect
+  });
 });
